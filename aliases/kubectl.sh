abfe86d8ec06d8d259d6f16d31c697f6312d62d1
Islam Shehata <islam.shehata@friday.de>
2020-04-14 15:54:59 +0200

    extend kubectl aliases

--- aliases/kubectl.sh
+++ aliases/kubectl.sh
@@ -2,10 +2,10 @@
 alias k=kubectl
 alias kl="kubectl logs -f"
 alias kg="kubectl get"
+alias ke="kubectl edit"
 alias kgp="kubectl get pod"
 alias kgi="kubectl get ingress"
-alias kgi="kubectl get ingress"
-alias ke="kubectl edit"
+alias kgs="kubectl get service"
 alias kei="kubectl edit ingress"
 alias ked="kubectl edit deployment"
 alias kes="kubectl edit service"
